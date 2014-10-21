c0c135a449d1d3022e8bbedb07a0e383a470aa07
alu0100611298 <alu0100611298@ull.edu.es>
2014-10-21 18:56:41 +0100

    base de datos en las pruebas

--- test/test.rb
+++ test/test.rb
@@ -12,3 +12,21 @@ include Rack::Test::Methods
 def app
 	Sinatra::Application
 end
+
+configure :test do
+	DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
+end
+
+DataMapper::Logger.new($stdout, :debug)
+DataMapper::Model.raise_on_save_failure = true 
+
+require_relative '../model'
+
+DataMapper.finalize
+
+#DataMapper.auto_migrate!
+DataMapper.auto_upgrade!
+
+describe "shortened urls" do
+	
+end
